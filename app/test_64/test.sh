2cba836a116ad47fe69780024d5cd35197ecf532
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-11-05 16:43:31 +0100

    Remove old unused and unusable things

--- app/test_64/test.sh
+++
@@ -1,7 +0,0 @@
-echo "<?xml version=\"1.0\" ?>
-<OmniRpcConfig>
-<Host name=\"127.0.0.1\" arch=\"i386\" os=\"linux\">
-<Agent invoker=\"mpi\" />
-<JobScheduler type=\"rr\" maxjob=\"1000\" />
-</Host>
-</OmniRpcConfig>"
