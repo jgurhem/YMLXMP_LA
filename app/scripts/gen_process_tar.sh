8c74f2bdfe71465c8316c5ff87dbdab3b7668db7
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-11-06 11:57:08 +0100

    Remove run files to process run1 at each run

--- app/scripts/gen_process_tar.sh
+++ app/scripts/gen_process_tar.sh
@@ -5,8 +5,13 @@ size=$3
 procs=$4
 
 echo "
+rm -rf run1/data
+rm core*
+
 mkdir -p ~/res_campaign/$app
 nb=\$[\$(ls ~/res_campaign/$app/$blocks-$size-$procs* | wc -l)+1]
 tar -cvf ~/res_campaign/$app/$blocks-$size-$procs---\$nb.tar.gz run* out-run-$app.*
+
+rm -rf run*
 "
 
