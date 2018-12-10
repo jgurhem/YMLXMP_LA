10e7e6f801b1dd563c7e65552e56c34bf8af9a64
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-12-10 19:47:09 +0100

    Put the code to process the log file into the process script instead of calling a external script

--- app/scripts/gen_process_analyse.sh
+++ app/scripts/gen_process_analyse.sh
@@ -16,7 +16,13 @@ echo "
 echo -n \"$machine;$nbhosts;$nbnodes;$app;$blocks;$size;$procs;\" >> $rf
 echo -n \$(date +%Y%m%d-%H%M%S) >> $rf
 
-. scripts/check_clear run1/exec_log
+file=run1/exec_log
+nbWorker=\$(grep pid=* \$file | cut -d '(' -f2 | sort | uniq -c | wc -l)
+tmpGen=\$(grep -E 'start *|end *' \$file -A 1 | grep -e '(' | cut -d '(' -f2 | tr -d ')' | awk '{x[NR]=\$1}END{asort(x);print x[NR]-x[1]}')
+totGen=\$(grep elasped* \$file | grep time: | cut -d ' ' -f3 | awk '{s+=\$1} END {print s}')
+tmpNoG=\$(grep -v ^host \$file | sed -E '/genMat|fillMatrixZero|genVect/,+9d' | grep -E 'start *|end *' -A 1 | grep -e '(' | cut -d '(' -f2 | tr -d ')' | awk '{x[NR]=\$1}END{asort(x);print x[NR]-x[1]}')
+totNoG=\$(grep -v ^host \$file | sed -E '/genMat|fillMatrixZero|genVect/,+9d' | grep elasped* | grep time: | cut -d ' ' -f3 | awk '{s+=\$1} END {print s}')
+nbTask=\$(grep -c SUCCESS \$file)
 
 echo -n \";\$nbWorker;\$tmpGen;\$totGen;\$tmpNoG;\$totNoG;\" >> $rf
 
