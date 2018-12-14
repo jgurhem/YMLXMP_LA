7dd7ac53cfb8f61490ef3fad59d4fdcd74ec3e73
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-12-14 10:54:26 +0100

    find last created run file instead of processing the first

--- app/scripts/gen_process_analyse.sh
+++ app/scripts/gen_process_analyse.sh
@@ -16,7 +16,9 @@ echo "
 echo -n \"$machine;$nbhosts;$nbnodes;$app;$blocks;$size;$procs;\" >> $rf
 echo -n \$(date +%Y%m%d-%H%M%S) >> $rf
 
-file=run1/exec_log
+d=\$(find . -maxdepth 1 -type d -name 'run*' -ls | sort -r | head -n 1 | awk '{print \$NF}')
+
+file=\$d/exec_log
 nbWorker=\$(grep pid=* \$file | cut -d '(' -f2 | sort | uniq -c | wc -l)
 tmpGen=\$(grep -E 'start *|end *' \$file -A 1 | grep -e '(' | cut -d '(' -f2 | tr -d ')' | awk '{x[NR]=\$1}END{asort(x);print x[NR]-x[1]}')
 totGen=\$(grep elasped* \$file | grep time: | cut -d ' ' -f3 | awk '{s+=\$1} END {print s}')
@@ -26,7 +28,7 @@ nbTask=\$(grep -c SUCCESS \$file)
 
 echo -n \";\$nbWorker;\$tmpGen;\$totGen;\$tmpNoG;\$totNoG;\" >> $rf
 
-if [ -f run1_results.pack ]
+if [ -f \"\$d\"_results.pack ]
 then
 	echo -n true >> $rf
 else
--- app/scripts/gen_process_tar.sh
+++ app/scripts/gen_process_tar.sh
@@ -5,13 +5,13 @@ size=$3
 procs=$4
 
 echo "
-rm -rf run1/data
+d=\$(find . -maxdepth 1 -type d -name 'run*' -ls | sort -r | head -n 1 | awk '{print \$NF}')
+
+rm -rf \$d/data
 rm core*
 
 mkdir -p ~/res_campaign/$app
 nb=\$[\$(ls ~/res_campaign/$app/$blocks-$size-$procs* | wc -l)+1]
-tar -cvf ~/res_campaign/$app/$blocks-$size-$procs---\$nb.tar.gz run* out-run-$app.*
-
-rm -rf run*
+tar -cvf ~/res_campaign/$app/$blocks-$size-$procs---\$nb.tar.gz \$d out-run-$app.*
 "
 
